f685b428c1ef909e43045ac76af9ec3dd42aea56
thailsondiniz <thailsondiniz94@gmail.com>
2025-12-19 16:39:33 -0300

    update: foram adicionados novos campos ao schema do boss e do usuario

--- schemas/boss.js
+++ schemas/boss.js
@@ -1,13 +1,42 @@
+const { text } = require("express");
 const mongoose = require("mongoose");
 
 const bossSchema = new mongoose.Schema({
   name: { type: String, required: true },
+  maxHealth: { type: Number, required: true },
   health: { type: Number, required: true },
-  phrases: { type: [String], required: true },
+  phrases: { type: [
+    {
+        text: String,
+        triggerPercentage: Number
+    }
+  ], required: true },
   imageBoss: { type: String, required: true },
   backgroundImage: { type: String, required: true },
-  lastHit: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
+  defeated: { type: Boolean, default: false },
+  lastHitUserId: { type: String, default: null },
+  topDamage: {
+    type: [
+      {
+        userId: String,
+        damage: Number,
+      },
+    ],
+    default: [],
+  },
+  participants: {
+    type: [
+      {
+        userId: String,
+        name: String,
+        damage: Number,
+        clicks: Number,
+      },
+    ],
+    default: [],
+  },
+  totalParticipants: { type: Number, default: 0 },
+  createdAt: { type: Date, default: Date.now },
 });
 
-module.exports = mongoose.model("Boss", bossSchema);;
+module.exports = mongoose.model("Boss", bossSchema);
--- schemas/user.js
+++ schemas/user.js
@@ -4,7 +4,25 @@ const User = new mongoose.Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
     email: { type: String, required: true },
-    createdAt: { type: Date, default: Date.now }
+    avatar: { type: String, required: false },
+    level: { type: Number, default: 1 },
+    xpToNextLevel: { type: Number, default: 100 },
+    xp: { type: Number, default: 0 },
+    bossHistory: { type: [
+        { 
+            bossId: String,
+            name: String,
+            participated: Boolean,
+            damage: Number,
+            clicks: Number,
+            lastHit: Boolean,
+            damageDealt: Number, 
+            defeatedAt: Date 
+        }
+    ], default: [] },
+    baseDamage: { type: Number, default: 1},
+    createdAt: { type: Date, default: Date.now },
+    ranking: { type: Number, default: 0 }
 });
 
 
